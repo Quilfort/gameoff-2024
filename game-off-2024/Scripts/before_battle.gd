9fa25f25542184302ac7fb723a67bf0fb9adf5d9
Quilfort Frederiks <88137144+Quilfort@users.noreply.github.com>
2024-11-05 19:03:12 +0100

    Remove Even button, get Computer Data by string

--- game-off-2024/Scripts/before_battle.gd
+++ game-off-2024/Scripts/before_battle.gd
@@ -5,7 +5,9 @@ var MAX_NUMBER = 500
 var player_number = 0
 var game_started = false
 
-var computer = ComputerData.normal
+## Add Check to Default to Normal if Computer Name is not accesable
+var computer_player = "normal"
+var computer = ComputerData.get(computer_player)
 var computer_name = computer.name
 var computer_strategy = computer.computer_strategy.to_lower()
 
@@ -17,21 +19,6 @@ func setup_game():
 	print(computer)
 
 func _on_start_button_pressed() -> void:
-	if !game_started:
-		player_number = $GuessInput.text.to_int()
-		if player_number < 1 or player_number > MAX_NUMBER:
-			$StartLabel.text = "Please enter a number between 1 and %d." % MAX_NUMBER
-			return
-		game_started = true
-		BattleData.game_started = game_started
-		BattleData.player_number = player_number
-		ComputerData.computer_strategy = "normal"
-		$GuessInput.clear()
-		BattleData.MAX_NUMBER = MAX_NUMBER
-		get_tree().change_scene_to_file("res://Scenes/battle.tscn")
-
-
-func _on_even_button_pressed() -> void:
 	if !game_started:
 		player_number = $GuessInput.text.to_int()
 		if player_number < 1 or player_number > MAX_NUMBER:
